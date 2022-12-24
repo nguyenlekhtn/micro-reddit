249c038d5db40d6bfa75336f3ec8224e21cf244e
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-12-24 23:30:43 +0700

    add association from post to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :link, presence: true
+  validates :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   validates :name, uniqueness: true, presence: true, length: { in: 4..12 }
   validates :email, uniqueness: true, presence: true
 end
