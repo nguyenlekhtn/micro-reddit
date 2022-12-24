9d7ad5511beff7027ec38fc16dea951b170cc531
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-12-24 20:14:05 +0700

    remove user's password field

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_124152) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
