401515279c6e357bbc34733f6d6c08189a960810
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-12-25 00:35:11 +0700

    add association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  
+  has_many :comments
+
   validates :title, presence: true, length: { maximum: 20 }
   validates :link, presence: true
   validates :user, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
-  
+  has_many :comments
+
   validates :name, uniqueness: true, presence: true, length: { in: 4..12 }
   validates :email, uniqueness: true, presence: true
 end
